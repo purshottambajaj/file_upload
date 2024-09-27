97783cc323c31ba32859a1477e41397f5926b131
purshottambajaj <69904451+purshottambajaj@users.noreply.github.com>
2024-09-27 11:53:13 +0530

    FilePreview.jsx

--- file-up/src/components/FilePreview.jsx
+++ file-up/src/components/FilePreview.jsx
@@ -10,7 +10,7 @@ const FilePreview = ({ file }) => {
       return <audio controls src={URL.createObjectURL(file)} />;
     } else if (fileType.startsWith('video/')) {
       return <video controls width="300" src={URL.createObjectURL(file)} />;
-    } else if (fileType === 'application/pdf') {
+    } else if (fileType === 'application/pdf') { 
       return <embed src={URL.createObjectURL(file)} width="300" height="400" type="application/pdf" />;
     } else {
       return <p>File format not supported</p>;
