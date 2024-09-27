15b61a8073ce35cb2708f8b2d606336f9ba84581
purshottambajaj <69904451+purshottambajaj@users.noreply.github.com>
2024-09-27 11:54:37 +0530

    server.js

--- file-up/server/server.js
+++ file-up/server/server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
+
 mongoose.connect('mongodb://localhost:27017/authdb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
