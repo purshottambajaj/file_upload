bac62f9b43dfdf8dccac2feea2755c5afbcf977f
purshottambajaj <69904451+purshottambajaj@users.noreply.github.com>
2024-09-27 11:53:36 +0530

    AuthLogin.jsx

--- file-up/src/components/AuthLogin.jsx
+++ file-up/src/components/AuthLogin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
 const AuthLogin = ({ setUser }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
